d9defeafdaff87ed76849fed20577dfb6e37b326
Sree Krishna Theja B <sbannur@voceracommunications.com>
2023-03-10 00:48:43 +0530

    nav-bar

---
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import Button from '../Button/Button'
+// import Logo from '../Logo/Logo'
+import styles from './Navbar.module.css'
+
+function Navbar() {
+  return (
+    <div className={styles.navbar}>
+      
+    </div>
+  )
+}
+
+export default Navbar
