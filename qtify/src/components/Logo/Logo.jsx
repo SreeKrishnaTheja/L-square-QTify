f37297177c6fff30a62c3bae00ec9f96579016f5
Sree Krishna Theja B <sbannur@voceracommunications.com>
2023-03-10 01:04:58 +0530

    nav-bar-with-logo

---
+++ qtify/src/components/Logo/Logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Logo() {
+  return (
+    <img src={ require ("../../assets/logo.png") } width= 
+    {"67px"} height={"34x"} />
+  )
+}
+
+export default Logo
--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 // import Button from '../Button/Button'
-// import Logo from '../Logo/Logo'
+import Logo from '../Logo/Logo'
 import styles from './Navbar.module.css'
 
 function Navbar() {
   return (
     <div className={styles.navbar}>
-      
+      <Logo/>
     </div>
   )
 }
