15931ac76346512e6ac5797ad62dada11c0e496f
Sree Krishna Theja B <sbannur@voceracommunications.com>
2023-03-10 01:26:01 +0530

    button

--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-// import Button from '../Button/Button'
+ import Button from '../Button/Button'
 import Logo from '../Logo/Logo'
 import styles from './Navbar.module.css'
 
@@ -7,6 +7,7 @@ function Navbar() {
   return (
     <div className={styles.navbar}>
       <Logo/>
+      <Button>Give Feedback</Button>
     </div>
   )
 }
